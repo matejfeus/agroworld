6d9281499a26084c92bac0c9d65ede80d288849e
Stjeoan Ruklić <sr51641@fer.hr>
2024-08-06 21:54:26 +0200

    Test slideshow

--- src/components/Content/Slideshow.js
+++ src/components/Content/Slideshow.js
@@ -1,9 +1,6 @@
 import useEmblaCarousel from 'embla-carousel-react'
 import Autoplay from "embla-carousel-autoplay";
 import classes from "./Slideshow.module.css";
-import img1 from "../../assets/slider1.jpg";
-import img2 from "../../assets/slider2.jpg";
-import img3 from "../../assets/slider3.jpg";
 import {useEffect} from "react";
 
 const Slideshow = () => {
@@ -19,20 +16,26 @@ const Slideshow = () => {
                 'webpack-dev-server-client-overlay'
             );
             if (resizeObserverErr) {
+                console.log(resizeObserverErrDiv)
                 resizeObserverErr.setAttribute('style', 'display: none');
             }
             if (resizeObserverErrDiv) {
+                console.log(resizeObserverErrDiv)
                 resizeObserverErrDiv.setAttribute('style', 'display: none');
             }
         });
     }, []);
 
+    const image1 = "https://agroworld.hr/images/slider1.jpg";
+    const image2 = "https://agroworld.hr/images/slider1.jpg";
+    const image3 = "https://agroworld.hr/images/slider1.jpg";
+
     return (
         <div className={classes.embla} ref={emblaRef}>
             <div style={{overflow: "hidden"}}  className={classes.embla__container}>
-                <div className={classes.embla__slide}><img style={{backgroundSize: "cover", width: "inherit", height: "inherit", maxWidth: "inherit", maxHeight: "inherit"}} src={img1}/></div>
-                <div className={classes.embla__slide}><img style={{backgroundSize: "cover", width: "inherit", height: "inherit", maxWidth: "inherit", maxHeight: "inherit"}} src={img2}/></div>
-                <div className={classes.embla__slide}><img style={{backgroundSize: "cover", width: "inherit", height: "inherit", maxWidth: "inherit", maxHeight: "inherit"}} src={img3}/></div>
+                <div className={classes.embla__slide}><img style={{backgroundSize: "cover", width: "inherit", height: "inherit", maxWidth: "inherit", maxHeight: "inherit"}} src={image1}/></div>
+                <div className={classes.embla__slide}><img style={{backgroundSize: "cover", width: "inherit", height: "inherit", maxWidth: "inherit", maxHeight: "inherit"}} src={image2}/></div>
+                <div className={classes.embla__slide}><img style={{backgroundSize: "cover", width: "inherit", height: "inherit", maxWidth: "inherit", maxHeight: "inherit"}} src={image3}/></div>
             </div>
         </div>
     )
