56a37fca23a9b0b871a9c7df2f3437e20ecb1346
Stjeoan Ruklić <sr51641@fer.hr>
2024-08-09 22:03:53 +0200

    Update sliders

--- src/components/Content/Slideshow.js
+++ src/components/Content/Slideshow.js
@@ -1,5 +1,8 @@
 import {useEffect} from "react";
 import EmblaCarousel from "./EmblaCarousel";
+import slider1 from "../../assets/slider1.jpg";
+import slider2 from "../../assets/slider2.jpg";
+import slider3 from "../../assets/slider3.jpg";
 
 const Slideshow = () => {
 
@@ -22,11 +25,7 @@ const Slideshow = () => {
         });
     }, []);
 
-    const image1 = "https://agroworld.hr/images/slider1.jpg";
-    const image2 = "https://agroworld.hr/images/slider2.jpg";
-    const image3 = "https://agroworld.hr/images/slider3.jpg";
-
-    const slides = [image3, image1, image2];
+    const slides = [slider1, slider2, slider3];
     const options = {loop: true};
 
     return (
