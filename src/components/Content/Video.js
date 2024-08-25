4050824390b36552db76e4630a19714cd8c60450
Stjeoan Ruklić <sr51641@fer.hr>
2024-08-25 17:40:24 +0200

    Fix video styling

--- src/components/Content/Video.js
+++ src/components/Content/Video.js
@@ -3,14 +3,11 @@ import classes from "./Video.module.css";
 const Video = (props) => {
 
     return (
-        <div className={classes.container}>
-            <div className={classes.video}>
-                <iframe className={classes.video}
-                        src={props.url}
-                        title="YouTube video player" frameBorder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                        referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-            </div>
+        <div className={classes.video}>
+            <iframe className={classes.video}
+                    src={props.url}
+                    title="YouTube video player"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
         </div>
     );
 }
